4cb1a9adf837ec097e083130bd8359434e005a12
KALKIESHWAR <kalkieshwar11@gmail.com>
2025-05-23 14:45:47 +0530

    Adding TASK 8

---
+++ src/main/java/com/example/Helloworld.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Helloworld  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to Jenkins and Maven!");
+        scanner.close();
+    }
+}
